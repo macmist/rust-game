573da0f96f62ea624c83c88a064b8296e4571c05
Quentin Musy <quentin.musy@gmail.com>
2024-07-01 22:58:40 +0100

    chore: fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use bevy::{prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}};
-
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
 #[derive(Component)]
 struct Stuff {
-    name: String
+    name: String,
 }
 
 fn main() {
@@ -14,42 +16,43 @@ fn main() {
         .run();
 }
 
-
-fn setup(mut commands: Commands, 
+fn setup(
+    mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,) {
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-        material: materials.add(Color::BLUE),
-        transform: Transform::from_xyz(0., 0., 0.,),
-        ..default()
-    }, Stuff {name: "Coucou".to_string()} ));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
+            material: materials.add(Color::BLUE),
+            transform: Transform::from_xyz(0., 0., 0.),
+            ..default()
+        },
+        Stuff {
+            name: "Coucou".to_string(),
+        },
+    ));
 }
 
-fn movement(mut sprite_position: Query<(&mut Transform, &mut Stuff)>,
+fn movement(
+    mut sprite_position: Query<(&mut Transform, &mut Stuff)>,
     camera_query: Query<(&Camera, &GlobalTransform)>,
     windows: Query<&Window>,
+) {
+    let (camera, camera_transform) = camera_query.single();
 
-     ) {
-   
+    let Some(cursor_position) = windows.single().cursor_position() else {
+        return;
+    };
 
-        let (camera, camera_transform) = camera_query.single();
+    // Calculate a world position based on the cursor's position.
+    let Some(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+        return;
+    };
 
-        let Some(cursor_position) = windows.single().cursor_position() else {
-            return;
-        };
-
-
-    
-        // Calculate a world position based on the cursor's position.
-        let Some(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
-            return;
-        };
-
-        println!("there are {} sprintes", sprite_position.iter().count());
-        for ((mut transform,_)) in &mut sprite_position {
-            *transform = Transform::from_xyz(point.x, point.y, 0.);
-        }
-
+    println!("there are {} sprintes", sprite_position.iter().count());
+    for ((mut transform, _)) in &mut sprite_position {
+        *transform = Transform::from_xyz(point.x, point.y, 0.);
+    }
 }
